f6220017f2f0f72a0f0dec7383aaf98c928ef9e5
jhshar.moham <jhshar.moham@awatapu.school.nz>
2021-07-06 12:15:31 +1200

    Base

---
+++ 00_MMF_base.py
@@ -0,0 +1,29 @@
+# import statements
+
+
+# functions go here
+
+
+# -- Main Routine --
+
+# Set up dictionaries / lists needed to hold data
+
+# Ask the user if they have used the program before and show instructions if necessary
+
+# Loop to get ticket details
+
+    # Get name (Can't be blank)
+
+    # Get age (between 12 and 130)
+
+    # Calculate ticket price
+
+    # Loop to ask for snacks
+
+    # Calculate snack price
+
+    # Ask for payment method (and apply subcharge if necessary)
+
+# Calculate total sales and profit
+
+# Output data to text file
