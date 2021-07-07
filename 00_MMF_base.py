4b44a3577a860480e7564ab7bad24b1fc35dd8fa
jhshar.moham <jhshar.moham@awatapu.school.nz>
2021-07-07 13:07:09 +1200

    Base components

--- 00_MMF_base.py
+++ 00_MMF_base.py
@@ -3,6 +3,21 @@
 
 # functions go here
 
+# checks that ticket name is not blank
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        # If name is not blank, program continues
+        if response != "":
+            return response
+
+        # If name is blank, show error (& repeat loop)
+        else:
+            print("Sorry - this can't be blank, please enter your name")
+
 
 # -- Main Routine --
 
@@ -13,6 +28,7 @@
 # Loop to get ticket details
 
     # Get name (Can't be blank)
+    name = not_blank("Name: ")
 
     # Get age (between 12 and 130)
 
@@ -27,3 +43,7 @@
 # Calculate total sales and profit
 
 # Output data to text file
+# Functions go here
+
+
+
