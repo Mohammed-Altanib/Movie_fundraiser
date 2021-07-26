acae8dd1010d3fcec2477bb86f369326c160f85a
jhshar.moham <jhshar.moham@awatapu.school.nz>
2021-07-26 15:06:31 +1200

    WiP ticket loop function

--- 02_ticket_loop_v1.py
+++ 02_ticket_loop_v1.py
@@ -6,7 +6,7 @@ max_tickets = 5
 
 while name != "xxx" and ticket_count < max_tickets:
     name = input("Name: ")
-    if any(char.isdigit() for char in name):
+    if any(char.isdigit() for char in name) or name == "":
         print("Please enter only alphabetical characters in your name.")
     else:
         ticket_count += 1
---
+++ 02_ticket_loop_v2.py
@@ -0,0 +1,22 @@
+# initialise loop so that it runs at least once
+
+name = ""
+ticket_count = 0
+max_tickets = 5
+
+while name != "xxx" and ticket_count < max_tickets:
+    print("You have {} seats left".format(max_tickets - ticket_count))
+
+    # Get details
+    name = input("Name: ")
+    if name == "xxx":
+        continue
+    else:
+        ticket_count += 1
+        print()
+
+if ticket_count == max_tickets:
+    print("You have sold all available tickets!")
+else:
+    print("You have sold {} tickets. \n"
+          "There are {} seat(s) still available".format(ticket_count, max_tickets - ticket_count))
