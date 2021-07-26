8ba68907477b2f221581e82a97c9ec4d69a2fb9d
jhshar.moham <jhshar.moham@awatapu.school.nz>
2021-07-26 14:27:53 +1200

    WiP ticket loop function

--- 02_ticket_loop_v1.py
+++ 02_ticket_loop_v1.py
@@ -1,9 +1,10 @@
 # start of loop
 
+name = ""
 ticket_count = 0
 max_tickets = 5
 
-while ticket_count < max_tickets:
+while name != "xxx" and ticket_count < max_tickets:
     name = input("Name: ")
     if any(char.isdigit() for char in name):
         print("Please enter only alphabetical characters in your name.")
